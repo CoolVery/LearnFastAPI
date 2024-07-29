e4565647335a53e6c544cc3a95f916f45f3b51e4
CoolVery <georgekuklevwork@gmail.com>
2024-07-29 19:14:25 +0300

    Created a query that gets a slice of user data via input parameters

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ app = FastAPI(
     title="Start to learn:)"
 )
 
-@app.get("/")
-def hello():
-    return 'Hellow world!'
+data_users = [
+    {"id": 1, "role": "admin", "name": "Alice"},
+    {"id": 2, "role": "editor", "name": "Bob"},
+    {"id": 3, "role": "subscriber", "name": "Charlie"},
+    {"id": 4, "role": "moderator", "name": "Diana"},
+    {"id": 5, "role": "viewer", "name": "Ethan"}
+]
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    return [user for user in data_users if user.get('id') == user_id]
+
+@app.get("/user_cut")
+def get_cut_user(limit: int = 3, offset: int = 0):
+    return data_users[offset:limit]
