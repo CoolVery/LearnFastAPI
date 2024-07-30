0f7f5d490f34fd8221f8e35d557a323adbb262c7
CoolVery <georgekuklevwork@gmail.com>
2024-07-30 18:32:16 +0300

    Made a class with conditions for fields through the pydantic, and also added a method for adding a transaction record

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Start to learn:)"
@@ -25,3 +28,22 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, data_users))[0]
     current_user["name"] = new_name
     return {"status": 200, "source": current_user}
+
+data_trades = [
+    {"id": 1, "user_id": 1, "currency": "USD", "side": "buy", "price": 100.50, "amount": 1.5},
+    {"id": 2, "user_id": 1, "currency": "EUR", "side": "sell", "price": 200.75, "amount": 2.0},
+    {"id": 3, "user_id": 2, "currency": "GBP", "side": "buy", "price": 300.00, "amount": 3.5},
+    {"id": 4, "user_id": 3, "currency": "JPY", "side": "sell", "price": 400.25, "amount": 4.1},
+    {"id": 5, "user_id": 4, "currency": "AUD", "side": "buy", "price": 500.60, "amount": 2.8}
+]
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    data_trades.extend(trades)
+    return {"status": 200, "source": data_trades}
